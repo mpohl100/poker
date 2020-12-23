7aaf8e102b30728915738659744d754507a456a3
mpohl100 <mpohl100@googlemail.com>
2020-12-23 17:30:51 +0100

    preflop now UTG acts first.

--- game/cpp/src/Nlhe.cpp
+++ game/cpp/src/Nlhe.cpp
@@ -30,21 +30,22 @@ void Nlhe52::playHand()
         player.dealHoleCards(deck_.getHoleCards());
 
     // pre-flop play
-    if(bool finished = playRound(Stack(big_blind_), board, handHistory, pot); finished)
+    if(bool finished = playRound(3, Stack(big_blind_), board, handHistory, pot); finished)
         return;
 
     // flop play
     board.dealFlop(deck_.getFlop());
-    if(bool finished = playRound(Stack(0), board, handHistory, pot); finished)
+    if(bool finished = playRound(0, Stack(0), board, handHistory, pot); finished)
         return;
 
     // turn play
     board.dealTurn(deck_.getCard());
-    if(bool finished = playRound(Stack(0), board, handHistory, pot); finished)
+    if(bool finished = playRound(0, Stack(0), board, handHistory, pot); finished)
         return;
+
     // river play
     board.dealRiver(deck_.getCard());
-    if(bool finished = playRound(Stack(0), board, handHistory, pot); finished)
+    if(bool finished = playRound(0, Stack(0), board, handHistory, pot); finished)
         return;
 
     // show down
@@ -70,15 +71,20 @@ bool Nlhe52::ready(Stack amt, Board const& board)
                          [amt, &board](auto const& p){ return p.ready(amt, board);});
 }
 
-bool Nlhe52::playRound(Stack currentBet, Board const& board, HandHistory& handHistory, Pot& pot)
+bool Nlhe52::playRound(int starting_pos, Stack currentBet, Board const& board, HandHistory& handHistory, Pot& pot)
 {
+    int i = 0;
     while(not ready(currentBet, board))
+    {
+        int nb = 0;
         for(auto& player : players_)
         {
-            if( not player.hasHoleCards() )
+            if( not player.hasHoleCards() or (i == 0 and nb++ < starting_pos))
                 continue;
             currentBet = player.decide(pot, board, handHistory);
         }
+        i++;
+    }
     // if only one player has hole cards after the betting round, the hand is finished
     if(std::count_if(players_.begin(), players_.end(), [](const auto& p){ return p.hasHoleCards();}) == 1)
     {
